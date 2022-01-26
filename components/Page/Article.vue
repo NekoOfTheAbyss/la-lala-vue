6b0a7cb15e18a7796ec7a717d05b0da9b882bef1
Caligo <schattenetcaligo@gmail.com>
2022-01-26 23:00:04 +0530

    code blocks

--- components/Page/Article.vue
+++ components/Page/Article.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-2 flex flex-col justify-start items-start space-y-2 w-full">
       <nuxt-content
-        class="prose dark:prose-dark prose-md w-full"
+        class="prose dark:prose-dark prose-md prose-code:before:content-none prose-code:after:content-none w-full"
         :document="news"
       />
   </div>
