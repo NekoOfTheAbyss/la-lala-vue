4089edd7d7e098b821c98e7f52078e5cadd8c3d6
Caligo <schattenetcaligo@gmail.com>
2022-01-26 21:09:43 +0530

    docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Blog Of The Abyss',
+    title: 'Docs Of The Abyss',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
