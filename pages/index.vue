0b47eec95383e95f61ce3d8012631623cdf009f2
Caligo <schattenetcaligo@gmail.com>
2022-01-26 22:32:37 +0530

    fix homepage

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@
 <script>
 export default {
   async asyncData({ params, $content }) {
-    const article = await $content(`main`).fetch()
+    const article = await $content(`main/lala`).fetch()
     return {
       article,
     }
