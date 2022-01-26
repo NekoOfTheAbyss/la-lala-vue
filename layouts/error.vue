067ad561c42544002ceb3ba0dd7c68d043ffab42
Caligo <schattenetcaligo@gmail.com>
2022-01-26 22:31:05 +0530

    404

---
+++ layouts/error.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class = "text-lg text-center p-8">
+        This page has no content. And will probably never have any content unless NeTT thinks otherwise.
+    </div>
+</template>
