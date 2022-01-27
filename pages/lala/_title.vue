469c1e5e8c4d6a5f5a1629c1253fd8e68dcd5bbc
Caligo <schattenetcaligo@gmail.com>
2022-01-27 15:00:06 +0530

    testest

--- pages/lala/_title.vue
+++ pages/lala/_title.vue
@@ -73,6 +73,8 @@ export default {
       return new Date(d).toUTCString()
     },
     getResult() {
+      console.log(typeof this.lala.random[this.article.url])
+      console.log(typeof this.lala.random.string)
       this.result = this.lala.random[this.article.url] ? this.lala.random[this.article.url]() : this.lala[this.article.url] ? new this.lala[this.article.url]() : 'Idk what to add here'
     }
   },
